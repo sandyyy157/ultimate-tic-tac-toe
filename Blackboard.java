34ee68e4f674b76e221d03929af589f50ec80feb
ValeriaCafazzo <valeria.cafazzo@edu.tbz.ch>
2024-01-12 16:35:54 +0100

    world size

--- Blackboard.java
+++ Blackboard.java
@@ -16,6 +16,6 @@ public class Blackboard extends World
     public Blackboard()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(400, 400, 1); 
     }
 }
