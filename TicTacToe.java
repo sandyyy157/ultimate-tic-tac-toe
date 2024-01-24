ecdf41f97d4772dc8d4b8d4f89331b99a56bbcef
sandyyy157 <huynhsandy157@gmail.com>
2024-01-24 22:42:39 +0100

    Stoppt wenn X oder O gewinnt

---
+++ TicTacToe.java
@@ -0,0 +1,100 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class TicTacToe here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class TicTacToe extends World
+{
+    private boolean xTurn;
+    /**
+     * Constructor for objects of class TicTacToe.
+     * 
+     */
+    public TicTacToe()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(3, 3, 100); 
+        makeBoard();
+        xTurn = true;
+    }
+    
+    public boolean isXTurn(){
+        return xTurn;
+    }
+    
+    public void changeTurns() {
+        xTurn = !xTurn;
+    }
+    public void makeBoard() {
+        for (int x = 0; x < 3; x++) {
+            for (int y = 0; y < 3; y++) {
+                addObject(new BoardPiece(), x, y);
+            }
+        }
+    }
+    
+    public void act() {
+        //schauen ob X gewinnt
+        for (int x = 0; x <3; x++) {
+            for (int y = 0; y < 3; y++) {
+                if (getObjectsAt(x, y, X.class).size() > 0 &&
+                    getObjectsAt(x + 1, y, X.class).size() > 0 &&
+                    getObjectsAt(x + 2, y, X.class).size() > 0){
+                        showText("X gewinnt!", 3,3);
+                        Greenfoot.stop();
+                    }
+                if (getObjectsAt(x, y, X.class).size() > 0 &&
+                    getObjectsAt(x, y + 1, X.class).size() > 0 &&
+                    getObjectsAt(x, y + 2, X.class).size() > 0){
+                        showText("X gewinnt!", 3,3);
+                        Greenfoot.stop();
+                    }
+                if (getObjectsAt(x, y, X.class).size() > 0 &&
+                    getObjectsAt(x + 1, y + 1, X.class).size() > 0 &&
+                    getObjectsAt(x + 2, y + 2, X.class).size() > 0){
+                        showText("X gewinnt!", 3,3);
+                        Greenfoot.stop();
+                    }
+                if (getObjectsAt(x, y, X.class).size() > 0 &&
+                    getObjectsAt(x + 1, y - 1, X.class).size() > 0 &&
+                    getObjectsAt(x + 2, y - 2, X.class).size() > 0){
+                        showText("X gewinnt!", 3,3);
+                        Greenfoot.stop();
+                    }
+            }
+        }
+        // schauen ob O gewinnt
+        for (int x = 0; x <3; x++) {
+            for (int y = 0; y < 3; y++) {
+                if (getObjectsAt(x, y, O.class).size() > 0 &&
+                    getObjectsAt(x + 1, y, O.class).size() > 0 &&
+                    getObjectsAt(x + 2, y, O.class).size() > 0){
+                        showText("O gewinnt!", 3,3);
+                        Greenfoot.stop();
+                    }
+                if (getObjectsAt(x, y, O.class).size() > 0 &&
+                    getObjectsAt(x, y + 1, O.class).size() > 0 &&
+                    getObjectsAt(x, y + 2, O.class).size() > 0){
+                        showText("O gewinnt!", 3,3);
+                        Greenfoot.stop();
+                    }
+                if (getObjectsAt(x, y, X.class).size() > 0 &&
+                    getObjectsAt(x + 1, y + 1, O.class).size() > 0 &&
+                    getObjectsAt(x + 2, y + 2, O.class).size() > 0){
+                        showText("O gewinnt!", 3,3);
+                        Greenfoot.stop();
+                    }
+                if (getObjectsAt(x, y, O.class).size() > 0 &&
+                    getObjectsAt(x + 1, y - 1, O.class).size() > 0 &&
+                    getObjectsAt(x + 2, y - 2, O.class).size() > 0){
+                        showText("O gewinnt!", 3,3);
+                        Greenfoot.stop();
+                    }
+            }
+        }
+        
+    }
+}
---
+++ Xwin.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Xwin here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Xwin extends Actor
+{
+    /**
+     * Act - do whatever the Xwin wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
